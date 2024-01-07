eddc062cf860951a68ee0f99c905257ebadf9111
PythonScript1 <loussalmohammeddev@gmail.com>
2024-01-07 21:37:06 +0100

    adding config file

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php 
+// database params 
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'alpha');
+
+// app root 
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL root 
+define('URLROOT', 'http://localhost/LM.ALPHA');
+// site name
+define('SITENAME', 'XENO');
+// app version 
+define('APPVERSION', '1.0.0');
+?>
